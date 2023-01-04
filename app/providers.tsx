6b65ac874903c613f55384e36bd30adaa89124e4
degwinthegreat <the.colledge.dropout@gmail.com>
2023-01-04 10:53:20 +0900

    use system color mode

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,14 +3,16 @@
 import { ChakraProvider, ColorModeScript, extendTheme, Container } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
-const colors = {
+const config = {
   brand: {
     900: '#1a365d',
     800: '#153e75',
     700: '#2a69ac',
   },
+  initialColorMode: 'system',
+  useSystemColorMode: false,
 }
-const theme = extendTheme({ colors })
+const theme = extendTheme({ config })
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
